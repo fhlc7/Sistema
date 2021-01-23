02f413e674b9bade47824715cddda81417cc310f
Fabiano Henrique <fhlc7@hotmail.com>
2021-01-23 18:24:31 -0300

    Teste(s)

---
+++ src/testes/Teste.java
@@ -0,0 +1,13 @@
+package testes;
+
+import javax.swing.JOptionPane;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		String mensagem = "www.fabianosoft.wordpress.com";
+		System.out.println(mensagem);
+		JOptionPane.showMessageDialog(null, mensagem);
+	}
+	
+}

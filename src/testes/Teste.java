7241c0007fc75e25bc80ec99c7925a48f80c5057
Fabiano Henrique <fhlc7@hotmail.com>
2021-01-23 18:28:01 -0300

    Outro(s) Teste(s)

--- src/testes/Teste.java
+++ src/testes/Teste.java
@@ -5,9 +5,9 @@ import javax.swing.JOptionPane;
 public class Teste {
 
 	public static void main(String[] args) {
-		String mensagem = "www.fabianosoft.wordpress.com";
-		System.out.println(mensagem);
-		JOptionPane.showMessageDialog(null, mensagem);
+		String mensagem = "www.fabianosoft.wordpress.com"; 
+		System.out.println(mensagem); 
+		JOptionPane.showMessageDialog(null, mensagem); 
 	}
 	
 }
